af59b9fee2dbe5b11fc428d93181e1d1e7e39f2f
hieple971312 <100987093+hieple971312@users.noreply.github.com>
2022-08-31 16:30:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Ngọc Anh" && b=="23102001")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
